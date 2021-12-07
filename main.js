ce893fe810125ee86093599a2ca66b94c03266e1
Hassan998-cloud <81471046+Hassan998-cloud@users.noreply.github.com>
2021-12-06 19:46:12 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+statuss = "";
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -7,6 +8,21 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log('modelLoaded');
+    statuss = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error)
+    {
+      console.error(error);
+    }
+    console.log(results);
 }
 
 function draw(){
@@ -16,4 +32,10 @@ function draw(){
     noFill();
     stroke("#00bbff");
     rect(30,60,450,350);
+
+    fill("#ff0008");
+    text("Cat",320,120);
+    noFill();
+    stroke("#00f2ff");
+    rect(300,90,270,320);
 }
